6550a6c7db66614342983e0f2758777c4b1f7cf2
hyouka007web <hyouka6@atomicmail.io>
2026-08-23 13:14:09 +0000

    Align Kotlin stdlib with AGP compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,3 +47,11 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
+
+configurations.configureEach {
+    resolutionStrategy.force(
+        "org.jetbrains.kotlin:kotlin-stdlib:2.2.0",
+        "org.jetbrains.kotlin:kotlin-stdlib-jdk7:2.2.0",
+        "org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.2.0"
+    )
+}
