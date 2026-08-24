ab4f3fe8b6ebfdd74dd93d8c3e755eb1c7fcc3ca
hyouka007web <hyouka6@atomicmail.io>
2026-08-24 11:32:43 +0200

    Alle Dateien aus dem Repository gelöscht

--- build.gradle.kts
+++
@@ -1,8 +0,0 @@
-// NEXUS Browser — root build file
-plugins {
-    id("com.android.application") version "9.3.1" apply false
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
-}
